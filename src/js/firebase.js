3b4a35c2f4d1404c31b3b8005ae4b36f614d41a3
soberanesmajo <soberanesmajo@gmail.com>
2018-08-19 21:23:18 -0500

    agrega guardar datos en firebase

---
+++ src/js/firebase.js
@@ -0,0 +1,12 @@
+window.bitacora = {
+    initializeFirebase: () => {
+        firebase.initializeApp({
+            apiKey: "AIzaSyBAWO8ol104yLQHYu5tqnuH0H72byrKebQ",
+            authDomain: "bitacora-a3ecb.firebaseapp.com",
+            databaseURL: "https://bitacora-a3ecb.firebaseio.com",
+            projectId: "bitacora-a3ecb",
+            storageBucket: "bitacora-a3ecb.appspot.com",
+            messagingSenderId: "751390318429"
+        });
+    },
+};
